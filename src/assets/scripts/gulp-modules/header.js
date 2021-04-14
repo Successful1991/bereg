f60d5c992e15bfaf9e5a227ac97be877b7843bf2
Artem Tsys <19successful91@gmail.com>
2021-04-14 09:54:53 +0300

    desktop

--- src/assets/scripts/gulp-modules/header.js
+++ src/assets/scripts/gulp-modules/header.js
@@ -2,11 +2,13 @@ function init() {
   window.addEventListener('click', (e) => {
     const elem = document.querySelector('.select-items');
     if (!e.target.classList.contains('select-selected') && !elem.classList.contains('select-hide')) {
+      document.querySelector('.select-selected').classList.remove('select-arrow-active');
       elem.classList.add('select-hide');
     }
   });
 
-  document.querySelector('.select-selected').addEventListener('click', () => {
+  document.querySelector('.select-selected').addEventListener('click', (e) => {
+    e.target.classList.add('select-arrow-active');
     document.querySelector('.select-items').classList.remove('select-hide');
   });
 }
