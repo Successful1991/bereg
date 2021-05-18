4d8dcb1b8c5c60ae8a129f26c8901f9bf3b807f2
Artem Tsys <19successful91@gmail.com>
2021-05-18 08:28:56 +0300

    style filter

--- src/assets/scripts/libs/libs.js
+++ src/assets/scripts/libs/libs.js
@@ -1,3 +1,4 @@
 @@include('../../../../node_modules/bezier-easing/dist/bezier-easing.min.js')
 @@include('./jquery/jquery.min.js')
 @@include('./swiper/swiper.js')
+@@include('./ion.rangeSlider/js/ion.rangeSlider.js')
