f7c3101aaf92950abeb23415fb0fdb8eee31b670
Artem Tsys <19successful91@gmail.com>
2021-05-21 12:05:42 +0300

    responsive flat

--- src/assets/scripts/gulp-modules/flat.js
+++ src/assets/scripts/gulp-modules/flat.js
@@ -1,8 +1,50 @@
+function updateHoverFlat(containers, data) {
+  const wrap = containers;
+  wrap.type.innerHTML = data.type;
+  wrap.flat.innerHTML = data.rooms;
+  wrap.area.innerHTML = data.area;
+}
+
+function initPopup() {
+  $('[data-flat-image]').magnificPopup({
+    type: 'image',
+  });
+}
+
+function hoverDataHundler(hoverElement, dataContainers) {
+  if (hoverElement.tagName === 'polygon') {
+    const data = hoverElement.dataset;
+    updateHoverFlat(dataContainers, data);
+  } else {
+    Object.values(dataContainers).forEach((element) => {
+      const el = element;
+      el.innerHTML = element.dataset.defaultValue;
+    });
+  }
+}
+
+function initPlan(conf) {
+  fetch(conf['3d'])
+    .then(res => (res.ok ? res : Promise.reject(res)))
+    .then(() => {
+      $('.plan').addClass('plan-active');
+    }).catch(() => {
+      $('.plan').removeClass('plan-active');
+    });
+}
+
 function flatInit() {
+  const dataContainers = {
+    type: document.querySelector('[data-type="type"]'),
+    flat: document.querySelector('[data-type="flat"]'),
+    area: document.querySelector('[data-type="area"]'),
+  };
+
   const image = document.querySelector('[data-flat-image]');
   const pathImages = JSON.parse(image.dataset.src);
   const setSrcImage = (path) => {
     image.setAttribute('src', path);
+    image.setAttribute('data-mfp-src', path);
   };
   const updatePlanActiveText = (type) => {
     const prevActive = document.querySelector('.active[data-plane]');
@@ -11,11 +53,24 @@ function flatInit() {
     nextActive.classList.add('active');
   };
 
-  document.querySelector('.js-switch-btn').addEventListener('change', (event) => {
+  initPlan(pathImages);
+  const switchElem = document.querySelector('.js-switch-btn');
+  switchElem.addEventListener('change', (event) => {
     const type = (event.target.checked) ? '3d' : '2d';
     setSrcImage(pathImages[type]);
     updatePlanActiveText(type);
   });
+
+  const svgContainer = document.querySelector('[data-svg-container]');
+  svgContainer.addEventListener('mouseleave', (event) => {
+    hoverDataHundler(event.target, dataContainers);
+  });
+  svgContainer.addEventListener('mouseover', (event) => {
+    hoverDataHundler(event.target, dataContainers);
+  });
+
+  initPopup();
 }
+
 
 document.addEventListener('DOMContentLoaded', flatInit);
--- src/assets/scripts/libs/libs.js
+++ src/assets/scripts/libs/libs.js
@@ -1,4 +1,5 @@
 @@include('../../../../node_modules/bezier-easing/dist/bezier-easing.min.js')
 @@include('./jquery/jquery.min.js')
+@@include('./magnific-popup/dist/jquery.magnific-popup.js')
 @@include('./swiper/swiper.js')
 @@include('./ion.rangeSlider/js/ion.rangeSlider.js')
