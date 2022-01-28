792526327cb1b26ed37350724c3ea3dc76fd8ae2
unknown <mr.andrey@bigmir.net>
2022-01-28 15:48:32 +0200

    add sections scroll on desktop homepage

--- src/assets/scripts/gulp-modules/home.js
+++ src/assets/scripts/gulp-modules/home.js
@@ -1,3 +1,6 @@
+/* eslint-disable func-names */
+/* eslint-disable no-undef */
+/* eslint-disable object-shorthand */
 function init() {
   // eslint-disable-next-line no-undef
   const slider = new Swiper('.swiper-container', {
@@ -20,6 +23,40 @@ function init() {
 
   slider.on('activeIndexChange', (obj) => {
     document.querySelector('[data-current]').innerHTML = obj.realIndex + 1;
+  });
+
+
+  let prevSection = 0;
+  const isMobile = window.matchMedia('(max-width: 575px)').matches;
+
+  if (!isMobile) {
+    locoScroll.destroy();
+    setTimeout(() => {
+      document.querySelector('.page__inner').style.transform = '';
+    }, 5000);
+  }
+  // eslint-disable-next-line no-unused-expressions
+  !isMobile && $.scrollify({
+    section: 'section',
+    scrollSpeed: 2250,
+    standardScrollElements: 'footer',
+    // easing: "easeInOut",
+
+    before: function (e, list) {
+      const direction = (e - prevSection) * -1;
+      const tl = gsap.timeline({ paused: true });
+      tl.fromTo(
+        list[e][0].querySelectorAll('.template-content'),
+        { y: 50 * direction, autoAlpha: 0 },
+        {
+          y: 0, autoAlpha: 1, ease: 'power4.easeInOut', duration: 2.250, stagger: 0.1,
+        },
+        '<',
+      );
+      tl.play();
+      prevSection = e;
+    },
+    // after: function(e, next) { },
   });
 }
 
